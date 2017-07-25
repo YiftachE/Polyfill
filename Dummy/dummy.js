61fab65d2a51d31f44ff67e7b159586efb6a67c4
David-PC\David <daffa2>
2017-07-25 20:32:22 +0300

    pushing for itamar

--- Dummy/dummy.js
+++ Dummy/dummy.js
@@ -1,15 +1,18 @@
-var map = L.map('map').setView([-32, -32], 10);
+var map = L.map('map').setView([+31.814, +34.644], 15);
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-for(var i = -30;i>-33;i-=0.2)
-{
-    for(var j = -30;j>-33;j-=0.2)
-    {
+//31.8147358, 34.6448723
+//31.8147632, 34.6475974
+for (var i = 31.814; i <31.816 ; i += 0.002) {
+    for (var j = +34.644 ; j < 34.648; j += 0.002) {
         L.polygon([
-            [i,j],
-            [i-0.2, j],
-            [i-0.2, j-0.2],
-            [i, j -0.2]
-        ]).bindTooltip(`${(i).toFixed(1)}:${(j).toFixed(1)}`).addTo(map)
+            [i, j],
+            [i + 0.002, j],
+            [i + 0.002, j + 0.002],
+            [i, j + 0.002]
+        ]).bindTooltip(`${(i).toFixed(3)}:${(j).toFixed(3)}`).addTo(map)
     }
 }
 
@@ -18,24 +21,22 @@ function getCellIdFromLatAndLng(lat, lng) {
     var absLng = Math.abs(lng);
     var flat = 0;
     var flng = 0;
-    let MATH1 = Number(absLat.toString().substr(0,4)) ;
-    MATH1 * 10 % 10 % 2 === 0 ? flat = MATH1  : flat = (MATH1 * 10 -1) /10;
-    let MATH2 = Number(absLng.toString().substr(0,4)) ;
-    MATH2 * 10 % 10 % 2 === 0 ? flng = MATH2 :flng =(MATH2 * 10 -1) /10;
-    if (flat.toString().length == 2) flat = flat.toString() + ".0";
-    if (flng.toString().length == 2) flng = flng.toString() + ".0";
+    let MATH1 = (Number(absLat) * 100).toString().substr(0,6);
+    MATH1 * 10 % 10 % 2 === 0 ? flat = MATH1/ 100 : flat = (MATH1 * 10 - 1) / 1000;
+    let MATH2 = (Number(absLng) * 100).toString().substr(0,6);
+    MATH2 * 10 % 10 % 2 === 0 ? flng = MATH2/ 100 : flng = (MATH2 * 10 - 1) / 1000;
     // return {absLat:flat,absLng:flng}
-    if(lat < 0) flat = "-" + flat;
-    if(lng < 0) flng = "-" + flng;
+    if (lat < 0) flat = "-" + flat;
+    if (lng < 0) flng = "-" + flng;
     return `${flat}:${flng}`
 }
 
-map.on('click', function(e) {
-    var popLocation= e.latlng;
+map.on('click', function (e) {
+    var popLocation = e.latlng;
     console.log(popLocation)
-    var cellId = getCellIdFromLatAndLng(popLocation.lat,popLocation.lng);
+    var cellId = getCellIdFromLatAndLng(popLocation.lat, popLocation.lng);
     var popup = L.popup()
         .setLatLng(popLocation)
-        .setContent(`${popLocation.lat.toFixed(1)}:${popLocation.lng.toFixed(1)}<br/><br/><br/>${cellId}`)
+        .setContent(`${popLocation.lat.toFixed(3)}:${popLocation.lng.toFixed(3)}<br/><br/><br/>${cellId}`)
         .openOn(map);
 });
